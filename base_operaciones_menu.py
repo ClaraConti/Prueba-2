37d49968b2818aa446ad3b1d03243c8a2b2c2fa2
ContiClara <73904391@continental.edu.pe>
2023-04-23 14:53:56 -0500

    Primer commit. Codigo base

---
+++ base_operaciones_menu.py
@@ -0,0 +1,21 @@
+
+menu=("""Bienvenido escoge una opción
+[1]Suma
+[2]Resta
+[3]Multiplicación
+[4]Division)
+""")
+print(menu)
+opcion=input("Ingrese una opción: ")
+
+if(opcion == "1"):
+    pass
+elif(opcion == "2"):
+    pass
+elif(opcion == "3"):
+    pass
+elif(opcion == "4"):
+    pass
+else:
+    print("Error.Debes Ingresar una opción del 1 al 4 ")
+    print("*"*20)
